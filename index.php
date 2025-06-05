bf9efad8242c9e93e59d720988081784625d4b2a
Daniel Weidner <d.weidner@hks-agentur.de>
2025-06-05 17:51:43 +0200

    fix: plugin definition

--- index.php
+++ index.php
@@ -4,6 +4,4 @@ Kirby::plugin('hksagentur/seo', [
     'blueprints' => require __DIR__ . '/config/blueprints.php',
     'snippets' => require __DIR__ . '/config/snippets.php',
     'translations' => require __DIR__ . '/config/translations.php',
-    'collectionMethods' => require __DIR__ . '/config/methods/collection.php',
-    'fieldMethods' => require __DIR__ . '/config/methods/field.php',
 ]);
