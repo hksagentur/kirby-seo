8d0217c15943899e39dc0d60f272b2ae89e4f3fc
Daniel Weidner <d.weidner@hks-agentur.de>
2026-06-10 11:58:28 +0200

    feat: add sitemap support

---
+++ config/methods/page.php
@@ -0,0 +1,51 @@
+<?php
+
+use Hks\Seo\Toolkit\Path;
+
+return [
+    'inSitemap' => function (): bool {
+        $exclude = $this->content()->sitemapExclude()->toBool();
+
+        if ($exclude) {
+            return false;
+        }
+
+        $ignore = $this->kirby()->option('hksagentur.seo.sitemap.ignore');
+
+        if (is_callable($ignore) && $ignore($this)) {
+            return false;
+        }
+
+        if (is_array($ignore) && Path::match($this->id(), $ignore)) {
+            return false;
+        }
+
+        return $this->isListable();
+    },
+    'sitemapPriority' => function (): float {
+        $priority = $this->content()->sitemapPriority()->toFloat(-1);
+
+        if ($priority >= 0 && $priority <= 1) {
+            return $priority;
+        }
+
+        if ($this->isHomePage()) {
+            return 1.0;
+        }
+
+        return match ($this->depth()) {
+            1 => 0.8,
+            2 => 0.6,
+            default => 0.5,
+        };
+    },
+    'sitemapModified' => function (?string $format = null): ?string {
+        $timestamp = $this->content()->sitemapModified()->toTimestamp() ?: $this->modified();
+
+        if (! $timestamp) {
+            return null;
+        }
+
+        return $format ? date($format, $timestamp) : $timestamp;
+    },
+];
---
+++ src/Http/RobotsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hks\Seo\Http;
+
+use Kirby\Cms\App;
+use Kirby\Http\Response;
+
+class RobotsController
+{
+    public function __invoke(): Response|false
+    {
+        $kirby = App::instance();
+
+        $cache = $kirby->cache('hksagentur.seo');
+        $options = $kirby->option('hksagentur.seo');
+
+        $robots = $options['robots']['active'] ?? false;
+        $sitemap = $options['sitemap']['active'] ?? false;
+
+        if (! $robots) {
+            return false;
+        }
+
+        $text = $cache->get('robots');
+
+        if ($text) {
+            return new Response($text, 'text/plain');
+        }
+
+        $path = $sitemap
+            ? $options['sitemap']['path'] ?? 'sitemap.xml'
+            : null;
+
+        $disallow = $options['robots']['disallow'] ?? [];
+
+        $text = $kirby->snippet('seo/robots', [
+            'disallow' => $disallow,
+            'sitemap' => $path,
+        ], true);
+
+        $cache->set(
+            key: 'robots',
+            value: $text,
+            minutes: $options['robots']['cacheDuration'] ?? 24 * 60
+        );
+
+        return new Response($text, 'text/plain');
+    }
+}
