5a004ae829b9def867b42e5790a6f0f64e7fa638
Daniel Weidner <d.weidner@hks-agentur.de>
2026-06-10 12:15:32 +0200

    fix: escape xml declaration in sitemap snippet to prevent short_open_tag parse error

--- views/snippets/sitemap.php
+++ views/snippets/sitemap.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <urlset xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($items as $item): ?>
         <url>
